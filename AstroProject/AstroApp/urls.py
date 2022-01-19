bfe6df5ed45a01c92952a57c426aa0fc44ea090d
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-01-20 04:56:36 +0600

    Mars Photos Integration Updated

--- AstroProject/AstroApp/urls.py
+++ AstroProject/AstroApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .AstroInfo import issLocation, astroPhotoDay
+from .AstroInfo import issLocation, astroPhotoDay, marsPhotos
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('ISSLocation/', issLocation.isslocation, name='ISSLocation'),
     path('astroPhotoDay/', astroPhotoDay.astroPhotoOftheDay, name = 'astroPhotoDay'),
     path('astroPhotoYesterday/', astroPhotoDay.astroPhotoOfYesterday, name = 'astroPhotoOfYesterday'),
+    path('marsPhotos/', marsPhotos.marsPhotos, name='marsPhotos'),
 ]
--- main.py
+++ main.py
@@ -1,149 +1,18 @@
-#Import required libraries:
-import nasapy
-import os
 from datetime import datetime, timedelta
-import urllib.request
-from IPython.display import Image,display,Audio
-from gtts import gTTS
-from pandas import DataFrame
-#Initialize Nasa class by creating an object:
+import requests
 
-k = "523p5hPYHGzafYGLCkqa54kKMTV2vbP0XcPxkcLm"
-nasa = nasapy.Nasa(key = k)
-
-#Get today's date in YYYY-MM-DD format:
-
-d = datetime.today().strftime('%Y-%m-%d')
-yesterday = datetime.today() - timedelta(days=1)
-yesterday.strftime('%Y-%m-%d')
-#Get the image data:
-
-apod = nasa.picture_of_the_day(date=d, hd=True)
-apodYesterday = nasa.picture_of_the_day(date=yesterday, hd=True)
-
-# POINT A:
-# Check the media type available:
-if (apod["media_type"] == "image"):
-
-    # POINT B:
-    # Displaying hd images only:
-    if ("hdurl" in apod.keys()):
-
-        # POINT C:
-        # Saving name for image:
-        title = d + "_" + apod["title"].replace(" ", "_").replace(":", "_") + ".jpg"
-
-        # POINT D:
-        # Path of the directory:
-        image_dir = "./Astro_Images"
-
-        # Checking if the directory already exists?
-        dir_res = os.path.exists(image_dir)
-
-        # If it doesn't exist then make a new directory:
-        if (dir_res == False):
-            os.makedirs(image_dir)
-
-        # If it exist then print a statement:
-        else:
-            print("Directory already exists!\n")
-
-        # POINT E:
-        # Retrieving the image:
-        urllib.request.urlretrieve(url=apod["hdurl"], filename=os.path.join(image_dir, title))
-
-        # POINT F:
-        # Displaying information related to image:
-
-        if ("date" in apod.keys()):
-            print("Date image released: ", apod["date"])
-            print("\n")
-        if ("copyright" in apod.keys()):
-            print("This image is owned by: ", apod["copyright"])
-            print("\n")
-        if ("title" in apod.keys()):
-            print("Title of the image: ", apod["title"])
-            print("\n")
-        if ("explanation" in apod.keys()):
-            print("Description for the image: ", apod["explanation"])
-            print("\n")
-        if ("hdurl" in apod.keys()):
-            print("URL for this image: ", apod["hdurl"])
-            print("\n")
-
-        # POINT G:
-        # Displaying main image:
-        display(Image(os.path.join(image_dir, title)))
-
-        # Point H:
-        # Text to Speech Conversion:
-        # Take input from user:
-        print("\n")
-        choice = input("Press * to hear the audio explanation : ")
-# POINT I:
-# If media type is not image:
+date = datetime.today() - timedelta(days=7)
+date.strftime('%Y-%m-%d')
+url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=' + (str)(
+    date) + '=2015-6-3&api_key=q85qDjg2dRNIebCwToX523S7jqmrfZYcDrnQCZzk'
+response = requests.get(url).json()
+# print(response['photos'][2]['name'])
+images = response['photos']
+size = len(images)
+nasa_photos = []
+if (size >= 10):
+    for i in range(10):
+        nasa_photos.append(images[i]['img_src'])
 else:
-    print("Sorry, Image not available!")
-
-if (apodYesterday["media_type"] == "image"):
-
-    # POINT B:
-    # Displaying hd images only:
-    if ("hdurl" in apod.keys()):
-
-        # POINT C:
-        # Saving name for image:
-        title = d + "_" + apodYesterday["title"].replace(" ", "_").replace(":", "_") + ".jpg"
-
-        # POINT D:
-        # Path of the directory:
-        image_dir = "./Astro_Images"
-
-        # Checking if the directory already exists?
-        dir_res = os.path.exists(image_dir)
-
-        # If it doesn't exist then make a new directory:
-        if (dir_res == False):
-            os.makedirs(image_dir)
-
-        # If it exist then print a statement:
-        else:
-            print("Directory already exists!\n")
-
-        # POINT E:
-        # Retrieving the image:
-        urllib.request.urlretrieve(url=apod["hdurl"], filename=os.path.join(image_dir, title))
-
-        # POINT F:
-        # Displaying information related to image:
-
-        if ("date" in apodYesterday.keys()):
-            print("Date image released: ", apodYesterday["date"])
-            print("\n")
-        if ("copyright" in apodYesterday.keys()):
-            print("This image is owned by: ", apodYesterday["copyright"])
-            print("\n")
-        if ("title" in apodYesterday.keys()):
-            print("Title of the image: ", apodYesterday["title"])
-            print("\n")
-        if ("explanation" in apodYesterday.keys()):
-            print("Description for the image: ", apodYesterday["explanation"])
-            print("\n")
-        if ("hdurl" in apodYesterday.keys()):
-            print("URL for this image: ", apodYesterday["hdurl"])
-            print("\n")
-
-        # POINT G:
-        # Displaying main image:
-        display(Image(os.path.join(image_dir, title)))
-
-        # Point H:
-        # Text to Speech Conversion:
-        # Take input from user:
-        print("\n")
-        choice = input("Press * to hear the audio explanation : ")
-
-# POINT I:
-# If media type is not image:
-else:
-    print("Sorry, Image not available!")
+    for i in size:
+        nasa_photos.append(images[i]['img_src'])
