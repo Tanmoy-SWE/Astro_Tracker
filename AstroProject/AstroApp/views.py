0923daefbb82e5a58adb844eca9e9e1463affa4b
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-01-19 12:46:25 +0600

    Embedded trinket.io to generate graphics to website

--- AstroProject/AstroApp/views.py
+++ AstroProject/AstroApp/views.py
@@ -6,6 +6,9 @@ import time
 import geocoder
 
 # Create your views here.
+
+#For uploading turtle graphics to webpage I used https://trinket.io/
+#Link :- https://trinket.io/library/trinkets/b741024f65?go=1
 def home(request):
     url = "http://api.open-notify.org/astros.json"
     response = urllib.request.urlopen(url)
