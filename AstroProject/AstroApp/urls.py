1e9abe0952b1448fcb4e6a1756a8de39f0aa1b85
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-01-19 14:00:59 +0600

    Refactored code to split functionalities

--- AstroProject/AstroApp/urls.py
+++ AstroProject/AstroApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .AstroInfo import issLocation
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('ISSLocation/', issLocation.isslocation, name='ISSLocation')
 ]
--- AstroProject/AstroApp/views.py
+++ AstroProject/AstroApp/views.py
@@ -7,12 +7,6 @@ import geocoder
 
 # Create your views here.
 
-#For uploading turtle graphics to webpage I used https://trinket.io/
-#Link :- https://trinket.io/library/trinkets/b741024f65?go=1
 def home(request):
-    url = "http://api.open-notify.org/astros.json"
-    response = urllib.request.urlopen(url)
-    result = json.loads(response.read())
-    astronauts = result['number']
-    context = {'astronauts':astronauts}
-    return render(request, 'home.html', context)
+    return render(request, 'home.html')
+
